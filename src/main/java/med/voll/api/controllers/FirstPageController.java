35ddf9e73667aa9fe67ada359f0d61c4664f76c7
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-25 15:46:52 -0300

    First page controller

---
+++ src/main/java/med/voll/api/controllers/FirstPageController.java
@@ -0,0 +1,17 @@
+package med.voll.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FirstPageController {
+
+    @GetMapping
+    public ResponseEntity<String> greetingMethod(@RequestParam(value = "name", defaultValue = "User") String name) {
+        return ResponseEntity.ok().body(String.format("Hello %s!", name));
+    }
+}
