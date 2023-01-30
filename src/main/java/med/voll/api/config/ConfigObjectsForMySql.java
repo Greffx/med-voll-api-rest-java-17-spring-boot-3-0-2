b6f102ba70774c7c94b3e3d533a526918a959197
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-30 17:14:09 -0300

    Refactoring seed class & properties

--- src/main/java/med/voll/api/config/ConfigObjectsForMySql.java
+++ src/main/java/med/voll/api/config/ConfigObjectsForMySql.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.Arrays;
-
 @Configuration
 @Profile("mySqlWorkbench")
 public class ConfigObjectsForMySql {
@@ -25,8 +23,8 @@ public class ConfigObjectsForMySql {
     @Bean
     public void ObjectsForTests() {
 
-        medicRepository.deleteAll();
-        addressRepository.deleteAll();
+//        medicRepository.deleteAll();
+//        addressRepository.deleteAll();
 
         Address address = new Address(null, "1th Street", 915, "near that cool market", "Porto Alegre", "RS", 91260370, "Deodoro Street");
         Address address1 = new Address(null, "2th Street", 951, "near that cool church", "São Paulo", "SP", 91264671, "Cabral Street");
@@ -35,13 +33,13 @@ public class ConfigObjectsForMySql {
         Address address4 = new Address(null, "5th Street", 124, "near that cool shop", "France", "SZ", 31565770, "Petreka's Street");
 
 
-        Medic medic = new Medic(null, "Eduardo", "12342", "eduardo@gmail.com", "51993631620", MedicSpeciality.DERMATOLOGIST, address);
-        Medic medic1 = new Medic(null, "Pedro", "1234", "pedro@gmail.com", "41993631620", MedicSpeciality.CARDIOLOGIST, address1);
-        Medic medic2 = new Medic(null, "Jonas", "12345", "jonas@gmail.com", "71993631620", MedicSpeciality.GYNECOLOGIST, address2);
-        Medic medic3 = new Medic(null, "Gabriel", "123456", "gabriel@gmail.com", "11993631620", MedicSpeciality.ORTHOPEDIST, address3);
-        Medic medic4 = new Medic(null, "Alex", "32145", "alex@gmail.com", "51993631620", MedicSpeciality.DERMATOLOGIST, address4);
+        Medic medic = new Medic(null, "Eduardo", 23, "12342", "eduardo@gmail.com", "51993631620", MedicSpeciality.DERMATOLOGIST, address);
+        Medic medic1 = new Medic(null, "Pedro", 35, "1234", "pedro@gmail.com", "41993631620", MedicSpeciality.CARDIOLOGIST, address1);
+        Medic medic2 = new Medic(null, "Jonas", 45, "12345", "jonas@gmail.com", "71993631620", MedicSpeciality.GYNECOLOGIST, address2);
+        Medic medic3 = new Medic(null, "Gabriel", 29,  "123456", "gabriel@gmail.com", "11993631620", MedicSpeciality.ORTHOPEDIST, address3);
+        Medic medic4 = new Medic(null, "Alex", 38, "32145", "alex@gmail.com", "51993631620", MedicSpeciality.DERMATOLOGIST, address4);
 
-        medicRepository.saveAll(Arrays.asList(medic, medic1, medic2, medic3, medic4));
-        addressRepository.saveAll(Arrays.asList(address, address1, address2, address3, address4));
+//        medicRepository.saveAll(Arrays.asList(medic, medic1, medic2, medic3, medic4));
+//        addressRepository.saveAll(Arrays.asList(address, address1, address2, address3, address4));
     }
 }
