89cddb2e5a07b8b78b0aafd4dc173365e732a67a
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-31 17:27:19 -0300

    Adding new method mapper

--- src/main/java/med/voll/api/mapper/MedicMapper.java
+++ src/main/java/med/voll/api/mapper/MedicMapper.java
@@ -4,6 +4,7 @@ import med.voll.api.entities.Medic;
 import med.voll.api.entities.dto.FormMedicDTO;
 import med.voll.api.entities.dto.FormToUpdateMedicDTO;
 import med.voll.api.entities.dto.MedicDTO;
+import med.voll.api.entities.dto.MedicDetailedDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,10 @@ public class MedicMapper {
 
     public FormToUpdateMedicDTO fromMedicToFormMedicDTODetailed(Medic medic) {
         return mapper.map(medic, FormToUpdateMedicDTO.class);
+    }
+
+    public MedicDetailedDTO fromMedicToMedicDetailedDTO(Medic medic) {
+        return mapper.map(medic, MedicDetailedDTO.class);
     }
 
     public Medic fromFormMedicDTOToMedic(FormMedicDTO medic) {
