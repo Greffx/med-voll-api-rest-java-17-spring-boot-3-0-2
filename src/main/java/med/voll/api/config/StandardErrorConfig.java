a74f3306f105763172b3d8d1d93568ca9d801864
Eduardo Greff <eduardogreff0@gmail.com>
2023-02-02 00:35:50 -0300

    Exception handler controller

--- src/main/java/med/voll/api/config/StandardErrorConfig.java
+++ src/main/java/med/voll/api/config/StandardErrorConfig.java
@@ -2,8 +2,65 @@ package med.voll.api.config;
 
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Instant;
+
 @Configuration
 public class StandardErrorConfig {
 
+    private String messageError;
+    private String path;
+    private Integer statusCode;
+    private String typeOfError;
+    private Instant momentOfError;
 
+    public StandardErrorConfig() {
+    }
+
+    public StandardErrorConfig(String messageError, String path, Integer statusCode, String typeOfError, Instant momentOfError) {
+        this.messageError = messageError;
+        this.path = path;
+        this.statusCode = statusCode;
+        this.typeOfError = typeOfError;
+        this.momentOfError = momentOfError;
+    }
+
+    public String getMessageError() {
+        return messageError;
+    }
+
+    public void setMessageError(String messageError) {
+        this.messageError = messageError;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getTypeOfError() {
+        return typeOfError;
+    }
+
+    public void setTypeOfError(String typeOfError) {
+        this.typeOfError = typeOfError;
+    }
+
+    public Instant getMomentOfError() {
+        return momentOfError;
+    }
+
+    public void setMomentOfError(Instant momentOfError) {
+        this.momentOfError = momentOfError;
+    }
 }
--- src/main/java/med/voll/api/controllers/exception/ExceptionHandlerController.java
+++ src/main/java/med/voll/api/controllers/exception/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controllers.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import med.voll.api.config.StandardErrorConfig;
 import med.voll.api.domain.services.exceptions.MedicNotFoundException;
 import med.voll.api.domain.services.exceptions.PatientNotFoundException;
@@ -8,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
     @ExceptionHandler(value = {MedicNotFoundException.class, PatientNotFoundException.class})
-    public ResponseEntity<StandardErrorConfig> code404ExceptionHandler() {
-        StandardErrorConfig errorConfig = new StandardErrorConfig();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body();
+    public ResponseEntity<StandardErrorConfig> code404ExceptionHandler(HttpServletRequest request, PatientNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StandardErrorConfig("Id not found", request.getRequestURI(), HttpStatus.NOT_FOUND.value(), "Bad Request", Instant.now()));
     }
-
 }
