adb887e4f21e214cf1a1d22e39f2e63096db23ca
Eduardo Greff <eduardogreff0@gmail.com>
2023-02-02 16:33:08 -0300

    login & password DTO

---
+++ src/main/java/med/voll/api/domain/entities/dto/security/AuthenticationDataDTO.java
@@ -0,0 +1,28 @@
+package med.voll.api.domain.entities.dto.security;
+
+public class AuthenticationDataDTO {
+
+    private String login;
+    private String password;
+
+    public AuthenticationDataDTO(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
