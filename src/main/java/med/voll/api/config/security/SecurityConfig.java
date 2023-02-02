f3a329593c47d0e76c70276d9231d9cbe608d97f
Eduardo Greff <eduardogreff0@gmail.com>
2023-02-02 16:14:15 -0300

    Configuration of security

---
+++ src/main/java/med/voll/api/config/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package med.voll.api.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //to enable configuration to security
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable() //csrf to disable attacks from CROSS-SITE REQUEST FORGERY
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //turning stateless, not stateful, cuz this is restfull
+                .and().build();
+    }
+
+}
