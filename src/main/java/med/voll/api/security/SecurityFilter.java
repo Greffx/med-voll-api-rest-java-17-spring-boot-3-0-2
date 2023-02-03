f557aacfe9ae8473f95fd305a4732cfe146f8514
Eduardo Greff <eduardogreff0@gmail.com>
2023-02-03 17:51:16 -0300

    permission for endpoints

--- src/main/java/med/voll/api/security/SecurityFilter.java
+++ src/main/java/med/voll/api/security/SecurityFilter.java
@@ -4,9 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.repositories.UserRepository;
 import med.voll.api.domain.services.security.TokenService;
-import med.voll.api.security.exception.TokenNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,15 +20,22 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService service;
 
+    @Autowired
+    private UserRepository repository;
+
 
     @Override                        //to get request              //to send something              //represent chain of filters
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        service.getSubject(String.valueOf(recoverToken(request)));
+
+        if (recoverToken(request) != null) {
+            var user = repository.findByLogin(service.getSubject(String.valueOf(recoverToken(request))));
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()));
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private Object recoverToken(HttpServletRequest request) {
-        return (request != null) ?  request.getHeader("Authorization").replace("Bearer", "") : new TokenNotFoundException("Token can't be null.");
+        return (request.getHeader("Authorization") != null) ?  request.getHeader("Authorization").replace("Bearer", "") : null;
     }
 }
